1178de7cf60413fe13291820e0d6b2ddbf9d469d
Alex <105147640+at-k1921081@users.noreply.github.com>
2025-05-02 22:28:40 +0100

    Fixed dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://at-k1921081.github.io",
-  base: "/blog",
-  integrations: [preact()],
+	site: "https://at-k1921081.github.io",
+	base: "/blog",
 });
